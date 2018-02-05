827b1eed567ad5a666037f6c8c1a1b0dd8c6eb56
moldovso <sorin@hpe.com>
2018-02-05 18:14:03 +0200

    Cleanup
    Signed-off-by: moldovso <sorin@hpe.com>

--- cs-content-packager-sources/src/main/java/io/cloudslang/tools/utils/YamlUtils.java
+++ cs-content-packager-sources/src/main/java/io/cloudslang/tools/utils/YamlUtils.java
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.cloudslang.tools.utils;
 
 import org.apache.commons.lang3.StringUtils;
